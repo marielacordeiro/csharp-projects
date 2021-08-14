0bc66b800256b27d058f3932f4c14cf96e4a4bdc
marielacordeiro <marielapontes@icloud.com>
2021-08-14 20:17:42 -0300

    commit lab10

--- Laboratorio10/Program.cs
+++ Laboratorio10/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Laboratorio10
 {
@@ -6,7 +8,69 @@ namespace Laboratorio10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           List<Pessoa> pessoas = new List<Pessoa>
+            {
+                new Pessoa{Nome="Ana",DataNascimento=new DateTime(1980,3,14), Casada=true},
+                new Pessoa{Nome="Paulo",DataNascimento=new DateTime(1978,10,23), Casada=true},
+                new Pessoa{Nome="Maria",DataNascimento=new DateTime(2000,1,10), Casada=false},
+                new Pessoa{Nome="Carlos",DataNascimento=new DateTime(1999,12,12), Casada=false}
+            };
+
+            var linq1 = from p in pessoas
+                        where p.Casada && p.DataNascimento >= new DateTime(1980, 1, 1)
+                        select p;
+
+            foreach (var pessoa in linq1) {
+                Console.WriteLine(pessoa);
+            }
+
+            var linq2 = pessoas.Where(p => p.Casada && p.DataNascimento >= new DateTime(1980, 1, 1));
+
+            foreach (var pessoa in linq2) {
+                Console.WriteLine(pessoa);
+            }
+
+            var linq3 = from p in pessoas
+                        where p.Casada && p.DataNascimento > new DateTime(2020, 1, 1)
+                        select p.Nome;
+            linq3.ToList().ForEach(Console.WriteLine);
+
+            
+            var linq4 = pessoas.Where(p => p.Casada).Count();
+            Console.WriteLine(linq4);
+            /*
+            var linq5 = from p in pessoas
+                        group p by p.Casada;
+                        */
+            var linq5 = pessoas.GroupBy(p => p.Casada);
+            foreach (var g in linq5)
+            {
+                Console.WriteLine(g.Key);
+                foreach (var p in g)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+
+            var linq6 = from p in pessoas
+                        group p by p.Casada into grupoPessoas 
+                        select new {
+                            Categoria = grupoPessoas.Key ? "Casada" : "Solteira",
+                            Pessoas = grupoPessoas.ToList(),
+                            Quantidade = grupoPessoas.Count()
+                        };
+
+                foreach (var pessoa in linq6) {
+                    Console.WriteLine(pessoa);
+                    foreach( var x in pessoa.Pessoas) {
+                        Console.WriteLine(x);
+                    }
+                }
+            
+            var older = pessoas.OrderBy((pessoa) => pessoa.DataNascimento).First();
+            Console.WriteLine(older);
+            var youngestSingle = pessoas.Where(p => !p.Casada).OrderBy((pessoa) => pessoa.DataNascimento).Last();
+            Console.WriteLine(youngestSingle);
         }
     }
 }
